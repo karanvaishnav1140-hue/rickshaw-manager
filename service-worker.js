349036f4b49767c85ea3608bb8666d7e507a8d60
karanvaishnav1140-hue <karanvaishnav1140@gmail.com>
2026-03-03 13:50:36 +0530

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
-self.addEventListener('install', e => {
+self.addEventListener("install",e=>{
 e.waitUntil(
-caches.open('rickshaw-cache').then(cache => {
-return cache.addAll(['index.html']);
+caches.open("rickshaw-cache").then(cache=>{
+return cache.addAll(["index.html"]);
 })
 );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch",e=>{
 e.respondWith(
-caches.match(e.request).then(response => {
-return response || fetch(e.request);
+caches.match(e.request).then(response=>{
+return response||fetch(e.request);
 })
 );
 });
